aad8f2eca50273c5c6eb45b1a3dd6fff068c2a4a
burnandy93-rgb <burnandy93@gmail.com>
2026-06-27 10:13:36 -0700

    zip1

--- zip.py
+++ zip.py
@@ -2,3 +2,5 @@ names = ["rita", "josephine", "michel"]
 scores = [27, 26, 65]
 for names, scores in zip(names,scores):
     print(names, scores)
+# i in range (len(names)):
+   # print(names[i], scores[i])
